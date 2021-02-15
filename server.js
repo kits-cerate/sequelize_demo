f9d6e6c1c5ca4f98bf6ca5a15c68098ce697bf60
kits-create <hi.kitsuka@gmail.com>
2021-02-15 13:02:48 +0900

    modify

--- server.js
+++ server.js
@@ -5,28 +5,29 @@ console.log(fs.readdirSync("./"));
 
 const { usersModel } = require("./models");
 
-//Sample data
-const dateNow = () => {
-  let d = new Date();
-  let yyyy = d.getFullYear();
-  let MM = ("0" + (d.getMonth() + 1)).slice(-2);
-  let dd = ("0" + d.getDate()).slice(-2);
-  return `${yyyy}-${MM}-${dd}`;
-};
+//Create sample data
+// const dateNow = () => {
+//   let d = new Date();
+//   let yyyy = d.getFullYear();
+//   let MM = ("0" + (d.getMonth() + 1)).slice(-2);
+//   let dd = ("0" + d.getDate()).slice(-2);
+//   return `${yyyy}-${MM}-${dd}`;
+// };
 
-usersModel.create({
-  name: "testName",
-  group: "testGroup",
-  date: dateNow(),
-});
+// usersModel.create({
+//   name: "testName",
+//   group: "testGroup",
+//   date: dateNow(),
+// });
 
 const app = express();
 
 const port = process.env.PORT || 3030;
 
-app.get("/getuser", (req, res) => {
-  res.json("Request is successfull");
-  usersModel.findAll();
+app.get("/user", (req, res) => {
+  usersModel.findAll().then((data) => {
+    res.json(data);
+  });
 });
 
 app.listen(port, () => {
